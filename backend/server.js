4a83059cde06bc66d2a6a554ede4bf97c3d9912c
VEVE-code <kzt2288330022@gmail.com>
2025-12-27 00:03:03 +0700

    hello hello again

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use("/api/clerk", express.raw({ type: "*/*" }));
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://jetskiim-mb1m.vercel.app"
+    "https://jetskiim-lb5i.vercel.app"
   ],
   credentials: true
 }));
