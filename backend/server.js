d46d565d278e162303b99f8019f2cd8fbda1bad3
VEVE-code <kzt2288330022@gmail.com>
2025-12-27 00:45:13 +0700

    moshi moshi

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use("/api/clerk", express.raw({ type: "*/*" }));
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://jetskiim-lb5i.vercel.app"
+    "https://jetskii-newfrontend.vercel.app"
   ],
   credentials: true
 }));
